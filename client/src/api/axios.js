50cd1daa1b4ce99b68b6786f607db83fec9fe641
kefale-Dev24 <kefaleseifu@gmail.com>
2025-04-09 15:39:37 +0300

    axios url updated

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -12,7 +12,8 @@ export default axiosInstance; */
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:7700/api",
+  // baseURL: "http://localhost:7700/api",
+  baseURL: "https://evangadi-forum-2-1665.onrender.com/api",
   headers: { "Content-Type": "application/json" },
 });
 
